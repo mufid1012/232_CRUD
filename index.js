ce0a8b6e3d781455ade4d2393ed5e19561fadc01
mufid1012 <mufidghibran1012@gmail.com>
2025-10-21 10:03:53 +0700

    setup express server dan mysql2 connection

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+let mysql = require('mysql2');
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
