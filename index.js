fa9200ea232f1fc4bcd5b21b1d48b032400ecdac
mufid1012 <mufidghibran1012@gmail.com>
2025-10-21 10:07:35 +0700

    menghubungkan server Express ke database MySQL

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+const db = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '', 
+  database: 'Biodata',
+  port: 3307    
+});
